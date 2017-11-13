1eb351d6254eb1df35e543720d93b8d3c23d743d
DXSsssss <wwlzxukeqing@163.com>
2017-11-13 23:25:42 +0800

    database modify

--- models.py
+++ models.py
@@ -4,14 +4,9 @@ from werkzeug.security import generate_password_hash,check_password_hash
 class User(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
-    telephone = db.Column(db.String(11),nullable=False)
+    telephone = db.Column(db.String(50),nullable=False)
     username = db.Column(db.String(50),nullable=False)
     password = db.Column(db.String(100),nullable=False)
-    name = db.Column(db.String(64))
-    location = db.Column(db.String(100))
-    about_me = db.Column(db.Text)
-    member_since = db.Column(db.DateTime(),default=datetime.now)
-    last_seen = db.Column(db.DateTime(),default=datetime.now)
 
     def __init__(self, *args, **kwargs):
         telephone = kwargs.get('telephone')
