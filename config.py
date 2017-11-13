d7d74d9670cc91ef6ac26d25a77c631588b1e6e2
DXSsssss <wwlzxukeqing@163.com>
2017-11-13 19:32:33 +0800

    config mysql name

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 HOSTNAME = '127.0.0.1'
 PORT = '3306'
-DATABASE = 'zlktqa_demo'
+DATABASE = 'qincaikt_demo'
 USERNAME = 'root'
 PASSWORD = '123456'
 DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
